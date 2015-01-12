0250470533555b1edd87f2a6e40b89c74d18c760
lucaverhees <lucaverhees@gmail.com>
2015-01-12 12:20:03 +0100

    naam inludes map aangepast

---
+++ includes/header.php
@@ -0,0 +1,42 @@
+<head>
+    <link href="<?php echo $style; ?>" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo $font_awesome; ?>">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,300,400' rel='stylesheet' type='text/css'>
+    <script src="<?php echo $script; ?>"></script>
+
+    <!--[if !IE 7]>
+		<style type="text/css">
+			#wrap {display:table;height:100%}
+		</style>
+		<![endif]-->
+</head>
+
+<body>
+    <!-- WRAP --> 
+    <div class="wrap">
+        
+    <!-- HEADER AREA --> 
+    <header>
+        <div class="menu_login">
+            <div class="wrapper">
+                <div class="left">
+                    <a href="<?php echo $en; ?>"><div class="en">EN</div></a>
+                    <a href="<?php echo $nl; ?>"><div class="nl">NL</div></a>
+                </div>
+                <div class="right">
+                    <a href="<?php echo $uitloggen; ?>"><div class="right_item">Uitloggen</div></a>
+                    <a href="<?php echo $mijn_account; ?>"><div class="right_item">Welkom, Jaap Verhoeven</div></a>
+                </div>
+            </div>
+        </div>
+            
+        <div class="menu_main">
+            <div class="wrapper">
+                <a href="<?php echo $home; ?>"><img class="logo_stagepeer"  src="<?php echo $logo; ?>" alt="StagePeer" /></a>
+                <nav>
+                    <a href="<?php echo $hoe_werkt_het ; ?>"><div class="menu_item">Hoe werkt het?</div></a>
+                    <a href="<?php echo $home ; ?>"><div class="menu_item">Zoek een vacature</div></a>
+                    <a href="<?php echo $mijn_account ; ?>"><div class="menu_item">Mijn Account</div></a>
+                </nav>
+            </div>
+        </div>
