0cdb10bbc286838d2a1d30039897f6e767b34174
lucaverhees <lucaverhees@gmail.com>
2015-01-15 16:04:49 +0100

    aanpassingen luca

--- index.php
+++ index.php
@@ -27,7 +27,56 @@
         <div class="search-area">
             <div class="wrapper">
                 <h3>Een prachtige slogan.</h3>
-                <input type="text" class="search-field"  placeholder="Doorzoek de vacatures.."><a href="<?php echo $zoekresultaten; ?>"><i class="fa fa-search fa-2x"></i></a>
+                
+                <form action="<?php echo $zoekresultaten.'#searchbar'; ?>" method="post">
+                    
+                    <div class="search-bar">
+                        <input class="search" name="zoekveld" type="text" placeholder="Doorzoek de vacatures.."/>
+
+                        <br>
+
+                        <p>Omgeving: </p>
+                        <select class="filters" name="omgeving">
+                            <option value="alles">Selecteer...</option>
+                            <option value="Noord-Holland">Noord-Holland</option>
+                            <option value="Zuid-Holland">Zuid-Holland</option>
+                            <option value="Utrecht">Utrecht</option>
+                            <option value="Flevoland">Flevoland</option>
+                            <option value="Gelderland">Gelderland</option>
+                            <option value="Overijssel">Overijssel</option>
+                            <option value="Noord-Brabant">Noord-Brabant</option>
+                            <option value="Groningen">Groningen</option>
+                            <option value="Drenthe">Drenthe</option>
+                            <option value="Friesland">Friesland</option>
+                            <option value="Limburg">Limburg</option>
+                            <option value="Zeeland">Zeeland</option>
+                            <option value="Internationaal">Internationaal</option>
+                        </select>
+
+                        <p>Periode: </p>
+                        <select class="filters" name="duur">
+                            <option value="alles">Selecteer...</option>
+                            <option value="6 maanden">6 maanden</option>
+                            <option value="1 jaar">1 jaar</option>
+                            <option value="2 jaar">2 jaar</option>
+                            <option value="Anders">Anders</option>
+                        </select> 
+
+                        <p>Studierichting: </p>
+                        <select class="filters" name="opleiding">
+                            <option value="alles">Selecteer...</option>
+                            <option value="Informatica">WO - Informatica</option>
+                            <option value="Informatiekunde">WO - Informatiekunde</option>
+                            <option value="Informatie, Multimedia, Management">WO - Informatie, Multimedia, Management</option>
+                            <option value="Medische Informatiekunde">WO - Medische Informatiekunde</option>
+                            <option value="Kunstmatige Intelligentie">WO - Kunstmatige Intelligentie</option>
+                            <option value="Anders">Anders</option>
+                        </select>
+                    </div>
+
+                    <input class="submit" name="zoeken" type="submit" value="Zoeken">
+                </form>
+                
             </div>
         </div>
         
