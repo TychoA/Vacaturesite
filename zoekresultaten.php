0f528aa43a9bab3537b57f10a76f649a114c2712
lucaverhees <lucaverhees@gmail.com>
2015-01-15 16:11:55 +0100

    database inloggegevens aangepast

--- zoekresultaten.php
+++ zoekresultaten.php
@@ -3,8 +3,8 @@
     <!-- DATA BASE CONNECTIE -->
     <?php        
         try {
-            $db = new PDO('mysql:host=localhost;dbname=webperen;charset=utf8',
-                'root', 'root');
+            $db = new PDO('mysql:host=localhost;dbname=stagepeer;charset=utf8',
+                'luca', 'fez7cJpE');
         } catch(PDOException $ex) {
             die("Something went wrong while connecting to the database!");
         }
