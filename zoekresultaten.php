bc47c004e174e5073ba1b6056d518adf02ebd1d2
lucaverhees <lucaverhees@gmail.com>
2015-01-15 16:17:31 +0100

    inloggegevens database aangepast

--- zoekresultaten.php
+++ zoekresultaten.php
@@ -85,7 +85,7 @@
             if( isset($_POST['zoekveld']) && isset($_POST['omgeving']) && isset($_POST['duur']) && isset($_POST['opleiding']) ){
                 // Connect to database    
                 try {
-                    $db = new PDO('mysql:host=localhost;dbname=webperen;charset=utf8', 'root', 'root');
+                    $db = new PDO('mysql:host=localhost;dbname=stagepeer;charset=utf8', 'luca', 'fez7cJpE');
                     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
                 } catch(PDOException $ex) {
