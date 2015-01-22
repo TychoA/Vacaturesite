0b82dbbe5fbb02543e8c47ba2740f978a2cf1834
Danzonius <tycho1996@gmail.com>
2015-01-22 11:22:06 +0100

    wat error statements toegevoegd

--- includes/connect.php
+++ includes/connect.php
@@ -3,6 +3,8 @@
         try {
             $db = new PDO('mysql:host=localhost;dbname=stagepeer;charset=utf8',
                 'luca', 'fez7cJpE');
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         } catch(PDOException $ex) {
             die("Something went wrong while connecting to the database!");
         }
