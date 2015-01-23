b8710b1b1319704a0af344496bbe4ccc4687714f
Marek <marekhoeven@gmail.com>
2015-01-23 16:24:29 +0100

    wachtwoord veranderen bij werknemers en werkgevers
    ja lees de titel maar gewoon

--- werknemers/instellingen.php
+++ werknemers/instellingen.php
@@ -2,11 +2,51 @@
 
     // Check of je ingelogd bent EN een werknemer bent, anders ga je naar de login_pagina.php
     if (isset($_SESSION['valid']) && (isset($_SESSION['werknemerid']) && !empty($_SESSION['werknemerid']))) {
-        $userID = $_SESSION['werknemerid'];
+         $userID = $_SESSION['werknemerid'];
     } else {
         header ('Location:../login_pagina.php');
     }
+    
 
+    if (isset($_POST['submit'])) {
+        $oud_ww = trim(strip_tags($_POST['old_password']));
+        $nieuw_ww = trim(strip_tags($_POST['new_password']));
+        $herhaal_ww = trim(strip_tags($_POST['new_password_again']));
+        
+        function checkOudWachtwoord($input) {
+            include '../includes/connect.php';
+            $sql_changepw = $db->prepare("SELECT wachtwoord FROM werknemers WHERE ID =:id LIMIT 1");
+            $sql_changepw ->execute(array(
+                'id' => $_SESSION['werknemerid']
+            )); 
+            $result =  $sql_changepw ->fetch();
+            $ww = $result['wachtwoord'];
+            
+            if ($ww == $input) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        
+        if ((!empty($oud_ww) && !empty($nieuw_ww) && !empty($herhaal_ww)) && ($nieuw_ww == $herhaal_ww) && checkOudWachtwoord($oud_ww)) {
+            // Updaten maar
+            include '../includes/connect.php';
+            $update_ww = "UPDATE werknemers
+                              SET wachtwoord = :new_wachtwoord
+                              WHERE ID = :userID";
+            $sth_ww = $db->prepare($update_ww);
+            $sth_ww->execute(array( 
+                ':new_wachtwoord' => $nieuw_ww,
+                ':userID' => $_SESSION['werknemerid']
+            ));    
+            
+            echo '<script>alert("Uw wachtwoord is succesvol veranderd!");</script>';
+        } else {
+            echo '<script>alert("Uw wachtwoord kon niet veranderd worden! Probeer het opnieuw.");</script>';
+        }
+    }
+    
 ?>
 <html>
     <?php include './linking.php';?>
@@ -40,10 +80,12 @@
             </p>
                 
             <div class="full edit_password">
-            	<h4>Oud wachtwoord</h4><input type="text" name="old_password" placeholder="..." />
-            	<h4>Nieuw wachtwoord</h4><input type="text" name="new_password" placeholder="..." />
-            	<h4>Herhaal nieuw wachtwoord</h4><input type="text" name="new_password_again" placeholder="..." />
-            	<input id="submit" type="submit" name="submit" value="Opslaan"
+                <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+            	    <h4>Oud wachtwoord</h4><input type="password" name="old_password" placeholder="..." />
+            	    <h4>Nieuw wachtwoord</h4><input type="password" name="new_password" placeholder="..." />
+            	    <h4>Herhaal nieuw wachtwoord</h4><input type="password" name="new_password_again" placeholder="..." />
+            	<input id="submit" type="submit" name="submit" value="Opslaan">
+            	</form>
             </div>
         </main>
     </div>
