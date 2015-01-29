ded00fd17bf5b2823ca1c9e8533f72363407f4d9
lucaverhees <lucaverhees@gmail.com>
2015-01-29 17:52:25 +0100

    Nieuwe tekst

--- hoe_werkt_het.php
+++ hoe_werkt_het.php
@@ -17,12 +17,10 @@
     <main>
         <div class="wrapper">
             <h1>Hoe werkt het?</h1>
-            <p>StagePeer is een website die speciaal gemaakt is voor studenten aan de Universiteit van Amsterdam en de Vrije Universiteit die studeren in de richting Informatiewetenschappen. Elke student heeft een afstudeerstage nodig om uiteindelijk de studie te kunnen voltooien. Het vinden van zo’n stage is vaak een moeilijk en langdurig proces. StagePeer is gemaakt om dit proces te vereenvoudigen en betrouwbaarder te maken.   De perfect passende stage is hier te vinden!></p>
-            <p>
-                Studenten kunnen een account aanmaken, hierin kunt een profiel worden opgezet met daarin alle eigenschappen en specificaties van de student. Dit profiel word gebruikt voor het vinden van een mogelijke match in de zoekmachine. Een account is tevens nodig om op de gevonden vacatures te kunnen reageren en zodoende in contact te komen met een mogelijk toekomstige werkgever.</p>
-
-            <p>
-    Werkgevers dienen ook een account aan te maken voordat er een vacature geplaatst kan worden. Deze aanmeldingen worden geverifieerd om misverstanden te kunnen voorkomen. Door het formele reageersysteem en het makkelijk vinden van de perfecte student voor uw open vacature is StagePeer de perfecte plek voor uw stageplaats!</p>   
+            <p class="first_alinea"><i><b>StagePeer</b></i> is een website die speciaal gemaakt is voor studenten aan de Universiteit van Amsterdam en de Vrije Universiteit die studeren in de richting Informatiewetenschappen. Elke student heeft een afstudeerstage nodig om uiteindelijk de studie te kunnen voltooien. Het vinden van zo’n stage is vaak een moeilijk en langdurig proces. <i><b>StagePeer</b></i> is gemaakt om dit proces te vereenvoudigen en betrouwbaarder te maken.  De perfect passende stage is hier te vinden!</p>
+            <img class="logo_groot" src="<?php echo $logo; ?>">
+        <p>Studenten kunnen een account aanmaken, hierin kunt een profiel worden opgezet met daarin alle eigenschappen en specificaties van de student. Dit profiel word gebruikt voor het vinden van een mogelijke match in de zoekmachine. Een account is tevens nodig om op de gevonden vacatures te kunnen reageren en zodoende in contact te komen met een mogelijk toekomstige werkgever.</p>
+        <p>Werkgevers dienen ook een account aan te maken voordat er een vacature geplaatst kan worden. Deze aanmeldingen worden geverifieerd om misverstanden te kunnen voorkomen. Door het formele reageersysteem en het makkelijk vinden van de perfecte student voor uw open vacature is <i><b>StagePeer</b></i> de perfecte plek voor uw stageplaats!</p>
         </div>
     </main>
 
